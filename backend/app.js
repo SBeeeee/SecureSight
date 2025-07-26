b6dda188c6d1415dd34ccd66cdbf63e496fea443
SBeeeee <shatadru017@gmail.com>
2025-07-26 21:38:54 +0530

    corse issue fixed

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000", // Allow your frontend origin
-    credentials: true, // If you're using cookies/sessions
-  }));
+  origin: [
+    "http://localhost:3000",
+    "https://secure-sight-coral.vercel.app"
+  ],
+  credentials: true,
+}));
 
 app.use("/api/incidents",incidentrouter);
 app.use("/api/camera",camerarouter);
