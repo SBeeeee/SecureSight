b05e77de6f1e956baea5671dba5178f0dbcafbef
SBeeeee <shatadru017@gmail.com>
2025-07-27 10:52:36 +0530

    added the color changes for figma

--- frontend/src/components/IncidentDetails.jsx
+++ frontend/src/components/IncidentDetails.jsx
@@ -1,33 +1,47 @@
 "use client";
 import { useEffect, useState } from "react";
-import { AlertTriangle, Plus, UserRoundSearchIcon, DoorOpen } from "lucide-react";
-import { fetchIncidents, resolveIncident,counts } from "./api";
-import { useDispatch,useSelector } from "react-redux";
-import { setIncidents,setResolved,setUnresolved } from "@/store/incidents/slice";
+import {
+  AlertTriangle,
+  Plus,
+  UserRoundSearchIcon,
+  DoorOpen,
+  Crosshair,
+  ChevronRight,
+} from "lucide-react";
+import { fetchIncidents, resolveIncident, counts } from "./api";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setIncidents,
+  setResolved,
+  setUnresolved,
+} from "@/store/incidents/slice";
 
 export default function IncidentDetails() {
-
-  const dispatch=useDispatch();
-  const {incidents,resolved,unresolved}=useSelector((state)=>state.incident)
+  const dispatch = useDispatch();
+  const { incidents, resolved, unresolved } = useSelector(
+    (state) => state.incident
+  );
   const [showResolved, setShowResolved] = useState(false);
 
   const loadIncidents = async () => {
     const data = await fetchIncidents(showResolved);
     dispatch(setIncidents(data));
   };
-  const handlecount=async()=>{
-    const data =await counts();
+
+  const handlecount = async () => {
+    const data = await counts();
     dispatch(setResolved(data.counts.resolved));
     dispatch(setUnresolved(data.counts.unresolved));
-  }
+  };
+
   useEffect(() => {
     loadIncidents();
     handlecount();
-  }, [showResolved,resolved,unresolved]);
+  }, [showResolved, resolved, unresolved]);
 
   const handleResolve = async (id) => {
     try {
-      await resolveIncident(id); 
+      await resolveIncident(id);
       await loadIncidents();
       await handlecount();
     } catch (err) {
@@ -35,20 +49,45 @@ export default function IncidentDetails() {
     }
   };
 
+  const getIconForType = (type) => {
+    switch (type) {
+      case "Unauthorised Access":
+        return <DoorOpen size={14} className="text-yellow-400" />;
+      case "Face Recognised":
+        return <UserRoundSearchIcon size={14} className="text-blue-400" />;
+      case "Gun Threat":
+        return <Crosshair size={14} className="text-red-500" />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div className=" pw-full md:w-[45%] h-full bg-[#1A1A1A] text-white p-4 rounded-md overflow-hidden">
+    <div className="pw-full md:w-[45%] h-full bg-[#1A1A1A] text-white p-4 rounded-md overflow-hidden">
       <div className="flex items-center justify-between mb-4 border-b border-gray-600 pb-2">
         <div className="flex items-center gap-1">
-          <AlertTriangle size={26} className="text-red-400 bg-red-900 rounded-full p-1" />
+          <AlertTriangle
+            size={26}
+            className="text-red-400 bg-red-900 rounded-full p-1"
+          />
           <h2 className="text-white font-semibold text-md">
             {showResolved ? `${resolved} Resolved` : `${unresolved} Unresolved`}
           </h2>
         </div>
 
         <div className="flex items-center gap-2">
-          <Plus size={16} className="text-white hover:text-yellow-400 cursor-pointer" />
-          <UserRoundSearchIcon size={16} className="text-white hover:text-yellow-400 cursor-pointer" />
-          <DoorOpen size={16} className="text-white hover:text-yellow-400 cursor-pointer" />
+          <Plus
+            size={16}
+            className="text-white hover:text-yellow-400 cursor-pointer"
+          />
+          <UserRoundSearchIcon
+            size={16}
+            className="text-white hover:text-yellow-400 cursor-pointer"
+          />
+          <DoorOpen
+            size={16}
+            className="text-white hover:text-yellow-400 cursor-pointer"
+          />
           <span
             onClick={() => setShowResolved((prev) => !prev)}
             className="text-white text-sm bg-black border-[#404040] border rounded-2xl px-2 py-1 cursor-pointer"
@@ -70,7 +109,10 @@ export default function IncidentDetails() {
               className="w-20 h-16 object-cover rounded"
             />
             <div className="flex-1 text-xs">
-              <div className="font-medium text-white">{incident.type}</div>
+              <div className="flex items-center gap-1 font-bold text-white">
+                {getIconForType(incident.type)}
+                {incident.type}
+              </div>
               <br />
               <div className="text-gray-400">Camera ID: {incident.cameraId}</div>
               <div className="text-gray-500">
@@ -88,10 +130,11 @@ export default function IncidentDetails() {
             </div>
 
             <button
-              className="text-blue-400 text-xs hover:underline cursor-pointer"
-              onClick={() => handleResolve(incident.id, showResolved)}
+              className="flex items-center gap-1 text-yellow-400 text-xs hover:underline cursor-pointer"
+              onClick={() => handleResolve(incident.id)}
             >
               {showResolved ? "Reopen" : "Resolve"}
+              <ChevronRight size={14} />
             </button>
           </div>
         ))}
